02e36c6539c6f3bcf5dd23d532a1bcb7e36d6749
Muqaddas Haideri <khanmuqaddas667@gmail.com>
2025-05-21 14:42:42 +0500

    made changes

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -42,6 +42,7 @@ export default function TabTwoScreen() {
         <ThemedText>
           You can open this project on Android, iOS, and the web. To open the web version, press{' '}
           <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
+          
         </ThemedText>
       </Collapsible>
       <Collapsible title="Images">
